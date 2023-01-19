5fe8322aab8747d34ea4b14998d8e5e6f279d583
esiammd <esiammd@hotmail.com>
2023-01-19 20:31:43 -0300

    first commit

---
+++ server/src/server.ts
@@ -0,0 +1,22 @@
+import Fastify from 'fastify'
+import cors from '@fastify/cors'
+import { PrismaClient } from '@prisma/client'
+
+async function bootstrap() {
+    const app = Fastify()
+    const prisma = new PrismaClient()
+
+    app.register(cors)
+
+    app.get('/', async () => {
+        const habits = await prisma.habit.findMany()
+
+        return habits
+    })
+
+    await app.listen({ port: 3333 }).then(() => {
+        console.log('HTTP Server running!')
+    })
+}
+
+bootstrap()
