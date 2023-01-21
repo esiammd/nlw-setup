e8fff97b0a1a08ba2d757902f6a8bea3d80bceb0
esiammd <esiammd@hotmail.com>
2023-01-21 15:11:00 -0300

    fix(web): update App component

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,14 +1,16 @@
-import './styles/global.css'
+import "./styles/global.css";
 
-import { Header } from './components/Header'
-
+import { Header } from "./components/Header";
+import { SummaryTable } from "./components/SummaryTable";
 
 export function App() {
   return (
-    <div className='w-screen h-screen flex justify-center items-center'>
-      <div className='w-full max-w-5xl px-6 flex flex-col gap-16'>
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className="w-full max-w-5xl px-6 flex flex-col gap-16">
         <Header />
+
+        <SummaryTable />
       </div>
     </div>
-  )
+  );
 }
