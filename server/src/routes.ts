1d3f5a41c2021a7de16b6897611cade14361c3a8
esiammd <esiammd@hotmail.com>
2023-01-25 15:09:48 -0300

    feat(server): days summary route creation

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -122,4 +122,26 @@ export async function appRoutes(app: FastifyInstance) {
             });
         }
     });
+
+    app.get('/summary', async (request) => {
+        // More complex query, more conditions, relationships => SQL by hand (RAW)
+        // Prisma ORM: RAW SQL => SQLite
+
+        const summary = await prisma.$queryRaw`
+            SELECT D.id, D.date, (
+                SELECT cast(count(*) as float)
+                FROM day_habits DH
+                WHERE DH.day_id = D.id
+            ) as completed, (
+                SELECT cast(count(*) as float)
+                FROM habit_week_days HWD
+                JOIN habits H ON H.id = HWD.habit_id
+                WHERE HWD.week_day = cast(strftime('%w', D.date/1000.0, 'unixepoch') as int)
+                    AND H.created_at <= D.date
+            ) as amount
+            FROM days D
+        `
+
+        return summary
+    });
 }
