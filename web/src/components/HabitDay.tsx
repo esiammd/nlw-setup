28efb6d8943becd630d61abf4328e9d5211357a9
esiammd <esiammd@hotmail.com>
2023-01-25 22:14:47 -0300

    feat(web): create popover

--- web/src/components/HabitDay.tsx
+++ web/src/components/HabitDay.tsx
@@ -1,5 +1,21 @@
+import * as Popover from '@radix-ui/react-popover';
+import { ProgressBar } from './ProgressBar';
+
 export function HabitDay() {
     return (
-        <div className="w-10 h-10 bd-zinc-900 border-2 border-zinc-800 rounded-lg"></div>
+        <Popover.Root>
+            <Popover.Trigger className="w-10 h-10 bd-zinc-900 border-2 border-zinc-800 rounded-lg">
+                <Popover.Portal>
+                    <Popover.Content className="min-w-[320px] p-6 rounded-2xl bg-zinc-900 flex flex-col">
+                        <Popover.Arrow height={8} width={16} className="fill-zinc-900" />
+                        
+                        <span className="front-semibold text-zinc-400">martes</span>
+                        <span className="mt-1 font-extrabold leading-tight text-3xl">25/01</span>
+                    
+                        <ProgressBar progress={75} />
+                    </Popover.Content>
+                </Popover.Portal>
+            </Popover.Trigger>
+        </Popover.Root>
     );
 }
