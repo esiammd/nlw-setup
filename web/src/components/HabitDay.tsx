78cb0bf846b643eea977265a535149f78656c4ac
esiammd <esiammd@hotmail.com>
2023-01-25 23:21:02 -0300

    fix(web): component update with the calculation of the percentage of completed habits

--- web/src/components/HabitDay.tsx
+++ web/src/components/HabitDay.tsx
@@ -1,10 +1,27 @@
 import * as Popover from '@radix-ui/react-popover';
+import clsx from 'clsx';
 import { ProgressBar } from './ProgressBar';
 
-export function HabitDay() {
+interface HabitDayProps {
+    completed: number;
+    amount: number;
+}
+
+export function HabitDay(props: HabitDayProps ) {
+    const completedPercentage = Math.round((props.completed / props.amount) * 100);
+
     return (
         <Popover.Root>
-            <Popover.Trigger className="w-10 h-10 bd-zinc-900 border-2 border-zinc-800 rounded-lg">
+            <Popover.Trigger
+                className={clsx("w-10 h-10 border-2 rounded-lg", {
+                    "bg-violet-900 border-violet-800": completedPercentage === 0,
+                    "bg-violet-900 border-violet-700": completedPercentage > 0 && completedPercentage < 20,
+                    "bg-violet-800 border-violet-600": completedPercentage >= 20 && completedPercentage < 40,
+                    "bg-violet-700 border-violet-500": completedPercentage >= 40 && completedPercentage < 60,
+                    "bg-violet-400 border-violet-500": completedPercentage >= 60 && completedPercentage < 80,
+                    "bg-violet-500 border-violet-400": completedPercentage >= 80,
+                })}
+            >
                 <Popover.Portal>
                     <Popover.Content className="min-w-[320px] p-6 rounded-2xl bg-zinc-900 flex flex-col">
                         <Popover.Arrow height={8} width={16} className="fill-zinc-900" />
@@ -12,7 +29,7 @@ export function HabitDay() {
                         <span className="front-semibold text-zinc-400">martes</span>
                         <span className="mt-1 font-extrabold leading-tight text-3xl">25/01</span>
                     
-                        <ProgressBar progress={75} />
+                        <ProgressBar progress={completedPercentage} />
                     </Popover.Content>
                 </Popover.Portal>
             </Popover.Trigger>
