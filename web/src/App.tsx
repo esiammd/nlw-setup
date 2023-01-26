79bc12bf36ed7c3a00da161a2b6370101b7ea812
esiammd <esiammd@hotmail.com>
2023-01-25 21:17:44 -0300

    feat(web): create modal in Header

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -8,7 +8,6 @@ export function App() {
     <div className="w-screen h-screen flex justify-center items-center">
       <div className="w-full max-w-5xl px-6 flex flex-col gap-16">
         <Header />
-
         <SummaryTable />
       </div>
     </div>
