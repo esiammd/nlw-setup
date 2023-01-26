1c9c36f8a53ceb67039622ee7cca5b8423192c1c
esiammd <esiammd@hotmail.com>
2023-01-25 21:32:50 -0300

    feat(web): creation of habit creation form

--- web/src/components/Header.tsx
+++ web/src/components/Header.tsx
@@ -2,6 +2,7 @@ import { Plus, X } from "phosphor-react";
 import * as Dialog from '@radix-ui/react-dialog';
 
 import logoImage from "../assets/logo.svg";
+import { NewHabitForm } from "./NewHabitForm";
 
 export function Header() {
     return (
@@ -28,6 +29,8 @@ export function Header() {
                 <Dialog.Title className="text-3xl leading-tight font-extrabold">
                   Crear Hábito
                 </Dialog.Title>
+
+                <NewHabitForm />
               </Dialog.Content>
             </Dialog.Portal>
           </Dialog.Root>
